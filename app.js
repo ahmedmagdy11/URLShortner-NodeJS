a84e88a194179cc133cc692dbbbba64e68af0c03
unknown <ahmed.magdy.9611@gmail.com>
2020-06-07 23:22:31 +0200

    minor css changes

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const app = express()
 const router = require('./routes/router')
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/URLS', {useNewUrlParser: true ,useUnifiedTopology:true})
+mongoose.connect('mongodb://localhost:27017/URLS', 
+{useNewUrlParser: true ,useUnifiedTopology:true},()=>{
+    console.log("connection established")
+})
 
-app.set('view engine' , 'ejs')
+
 app.use(express.urlencoded({extended:false}))
-
 app.use(router)
 
-app.listen((process.env.PORT || 5000 ));
+app.listen((process.env.PORT || 5000 ),()=>{
+    console.log(`app is listening on port ${5000}`)
+});
--- routes/router.js
+++ routes/router.js
@@ -1,28 +1,27 @@
-const express = require('express')
-const router= express()
-const urls = require('../models/urls')
+const express = require("express");
+const urls = require("../models/urls");
+const router = express();
 
-router.get('/',(req,res)=>{
-   
-    res.render('index.ejs',{shortUrl:null})
-})
+router.set('view engine' , 'ejs')
 
-router.post('/',async(req,res)=>{
-    await urls.create({fullUrl:req.body.FullUrl})
-    const query = await urls.findOne({fullUrl:req.body.FullUrl}) 
-    res.render('index.ejs',{shortUrl:query})
-})
+router.get("/", (req, res) => {
+  res.render("index.ejs", { shortUrl: null });
+});
 
-router.get('/:url',async(req,res)=>{
-   
-    const query = await urls.findOne({GeneratedUrl:req.params.url}) 
-  
-   if (query==null) {
-        res.sendStatus(404)
-   }
-   else { 
-    res.redirect(query.fullUrl)
-}
-})
+router.post("/", async (req, res) =>{
+  await urls.create({ fullUrl: req.body.FullUrl });
+  const query = await urls.findOne({ fullUrl: req.body.FullUrl });
+  res.render("index.ejs", { shortUrl: query });
+});
 
-module.exports=router
+router.get("/:url", async (req, res) => {
+  const query = await urls.findOne({ GeneratedUrl: req.params.url });
+
+  if (query == null) {
+    res.sendStatus(404);
+  } else {
+    res.redirect(query.fullUrl);
+  }
+});
+
+module.exports = router;
